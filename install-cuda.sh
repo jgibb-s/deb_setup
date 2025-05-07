8ac5f07e9a6ffbf6c21551546c7c13e464c52c91
jgibb-s <joshbtgibbs@gmail.com>
2025-05-07 10:50:42 -0700

    Fixed install-cuda.sh

--- install-cuda.sh
+++ install-cuda.sh
@@ -1,23 +1,33 @@
 #! /bin/bash
 
-# Largely from https://wiki.debian.org/NvidiaGraphicsDrivers
+# Largely from
+# https://wiki.debian.org/NvidiaGraphicsDrivers
+# https://docs.nvidia.com/cuda/cuda-installation-guide-linux/
+# https://docs.nvidia.com/datacenter/tesla/driver-installation-guide/index.html
 
 #add contrib non-free to source.list
-apt add
+sudo add-apt-repository non-free
+sudo add-apt-repository contrib
+sudo apt update
+
+# Check for gpu
+lspci -nn | egrep -i "3d|display|vga"
+
+#prompt continue?
+
+# Pre-reqs
+sudo apt install linux-headers-amd64 gcc nvidia-detect
+
 #confirm gcc installed
-
-apt install linux-headers-amd64
-
 gcc --version
 
-apt install nvidia-detect
 nvidia-detect
 
-apt install cuda-toolkit nvidia-driver nvidia-smi
+sudo apt install firmware-misc-nonfree nvidia-driver nvidia-smi nvidia-cuda-toolkit
 
 export PATH=/usr/local/cuda-12.6/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-12.6/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 
-mkdir /etc/X11/xorg.conf.d/10-intel.conf
-echo -e 'Section "Device"\n\tIdentifier "Intel GPU"\n\tDriver "intel"\nEndSection' > /etc/X11/xorg.conf.d/10-nvidia.conf
+touch /etc/X11/xorg.conf.d/10-intel.conf
+echo -e 'Section "Device"\n\tIdentifier "Intel GPU"\n\tDriver "intel"\nEndSection' > /etc/X11/xorg.conf.d/10-intel.conf
